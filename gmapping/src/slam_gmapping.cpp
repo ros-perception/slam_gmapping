26f3b8ba4c9da12a2a4cd761a70517c0105ddc55
Vincent Rabaud <vincent.rabaud@gmail.com>
2014-07-27 11:49:39 +0200

    fix typo that prevents compilation and fixes #18

--- gmapping/src/slam_gmapping.cpp
+++ gmapping/src/slam_gmapping.cpp
@@ -539,7 +539,7 @@ SlamGMapping::computePoseEntropy()
 void
 SlamGMapping::updateMap(const sensor_msgs::LaserScan& scan)
 {
-  boost::mutex::scoped_lock lock map_lock (map_mutex_);
+  boost::mutex::scoped_lock map_lock (map_mutex_);
   GMapping::ScanMatcher matcher;
   double* laser_angles = new double[scan.ranges.size()];
   double theta = angle_min_;
